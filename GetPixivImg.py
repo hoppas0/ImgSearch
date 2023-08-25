f016c173e4884fe6c130d92e16e3b4617fdf2bb5
hoppas0 <2622850487@qq.com>
2023-08-25 21:44:52 +0800

    You can name the downloaded image the original name on Pixiv

--- GetPixivImg.py
+++ GetPixivImg.py
@@ -1,26 +1,31 @@
 import os
 import time
 import requests
+import asyncio
+import aiohttp
 
 
-async def GetPixivImg(name:str, url:str):
+async def getPixivImg(url: str, name: str = ""):
     """
     通过pixiv网址下载图片
-    由于这是个搜索原图并下载的程序，为了方便看出原图片是否下载到原图，所以这里下载图片的名字就是原图片的名字
+    当name不为空时，就用name来为下载的图片命名，否则用Pixiv的名字
     :param name: str
     :param url: str
     :return: Int
     """
     # 得到返回的网页
     html = requests.get(url=url)
+    print("html:", html)
     # 从网页代码里找到目标图片的url
     url_list = html.text.split('"')
+    print("url_list", url_list)
     for url in url_list:
         if 'https://i.pximg.net/img-original/' in url:
             img_url = url
             break
     else:
         return 0
+    print("img_url", img_url)
     headers_download = {
         "referer": "https://www.pixiv.net/"
         # 这里携带referer因为p站的图片链接都是防盗链
@@ -30,6 +35,8 @@ async def GetPixivImg(name:str, url:str):
     # name = url.split("/")[-1]
     if not os.path.exists("PixivImage"):
         os.mkdir("PixivImage")
+    if name == "":      # 当name为空时，使用Pixiv原名
+        name = url.split("/")[-1]
     with open("./PixivImage/" + name, "wb") as f:
         f.write(response.content)  # 将图片二进制数据存入，图片也就得到了
     print(name + "下载成功")
@@ -38,6 +45,9 @@ async def GetPixivImg(name:str, url:str):
 
 
 if __name__ == "__main__":
-    name="test.jpg"
-    url="https://www.pixiv.net/artworks/97099960"
-    GetPixivImg(name, url)
+    name = "test.jpg"
+    # url = "https://www.pixiv.net/artworks/97099960"
+    url = "https://www.pixiv.net/artworks/74718584"
+    getPixivImg(name, url)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(getPixivImg(url,'123.jpg'))
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import base64
 import asyncio
-import threading
 import time
 
 from PIL import Image
@@ -9,11 +8,12 @@ import os.path
 from io import BytesIO
 from ImgSearch import ImgSearch
 from UrlToBase64 import urltobase64
-from GetPixivImg import GetPixivImg
+from GetPixivImg import getPixivImg
 import pyperclip
 
 apikey = ''
-selected_language = "Chinese"
+selectedLanguage = "Chinese"
+expectedName = 'fileName'
 languages = {
     'Chinese': {
         'SELECTLANGUAGE': '选择语言',
@@ -28,7 +28,10 @@ languages = {
         'copyUrl': '复制链接',
         'TIPS5': '注意：',
         'TIPS6': '搜图前请确认是否填写好apikey',
-        'TIPS7': '下载前请确认是否打开了代理'
+        'TIPS7': '下载前请确认是否打开了代理',
+        'rename': '下载的图片命名为:',
+        'filename': '原文件名',
+        'pixivname': '原Pixiv名'
     },
     'English': {
         'SELECTLANGUAGE': 'Select language',
@@ -43,13 +46,16 @@ languages = {
         'copyUrl': 'Copy the link',
         'TIPS5': 'Warning:',
         'TIPS6': 'Before searching for images,',
-        'TIPS7': 'please make sure that you fill in the apikey'
+        'TIPS7': 'please make sure that you fill in the apikey',
+        'rename': 'The downloaded picture is named:',
+        'filename': 'Original file name',
+        'pixivname': 'Original Pixiv name'
     }
 }
 
 
 async def run():
-    global apikey, languages, selected_language
+    global apikey, languages, selectedLanguage, expectedName
     file_list_column = [
         [sg.Text("选择语言", size=(20, 1), key='-SELECTLANGUAGE-'),
          sg.Combo(["Chinese", "English"], size=(10, 1), default_value="Chinese", enable_events=True, key='-LANGUAGE-')],
@@ -65,6 +71,10 @@ async def run():
                 values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
             )
         ],
+        [sg.Text("下载的图片命名为:", key='-rename-')],
+        # [sg.Radio('原文件名', "RADIO1", default=True, key='-filename-'), sg.Radio('原Pixiv名', "RADIO1",key='-pixivname-')],
+        [sg.Button('原文件名', key='-fileName-', button_color=('white', "#082567")),
+         sg.Button('    ', key='-pixivName-', button_color=('#082567', "#082567"))],
         [sg.Button(key="-download-", button_text="自动搜图并下载")],
         [sg.Text("这里的下载仅限于pixiv上的图片", key='-TIPS3-')],
         [sg.Text(size=(40, 1), key="-remaining-")],
@@ -103,26 +113,41 @@ async def run():
                 window["-apikey-"].update(apikey)
         except:
             window["-remaining-"].update(
-                "apikey.txt出错了" if selected_language == 'Chinese' else "APIkey .txt Something went wrong")
+                "apikey.txt出错了" if selectedLanguage == 'Chinese' else "APIkey .txt Something went wrong")
 
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
 
         if event == "-LANGUAGE-":
-            selected_language = values['-LANGUAGE-']
-            window['-SELECTLANGUAGE-'].update(languages[selected_language]['SELECTLANGUAGE'])
-            window['-TIPS1-'].update(languages[selected_language]['TIPS1'])
-            window['-TIPS2-'].update(languages[selected_language]['TIPS2'])
-            window['-FolderBrowse-'].update(languages[selected_language]['FolderBrowse'])
-            window['-download-'].update(languages[selected_language]['download'])
-            window['-TIPS3-'].update(languages[selected_language]['TIPS3'])
-            window['-TIPS4-'].update(languages[selected_language]['TIPS4'])
-            window['-similarity-'].update(languages[selected_language]['similarity'])
-            window['-author-'].update(languages[selected_language]['author'])
-            window['-copyUrl-'].update(languages[selected_language]['copyUrl'])
-            window['-TIPS5-'].update(languages[selected_language]['TIPS5'])
-            window['-TIPS6-'].update(languages[selected_language]['TIPS6'])
-            window['-TIPS7-'].update(languages[selected_language]['TIPS7'])
+            selectedLanguage = values['-LANGUAGE-']
+            window['-SELECTLANGUAGE-'].update(languages[selectedLanguage]['SELECTLANGUAGE'])
+            window['-TIPS1-'].update(languages[selectedLanguage]['TIPS1'])
+            window['-TIPS2-'].update(languages[selectedLanguage]['TIPS2'])
+            window['-FolderBrowse-'].update(languages[selectedLanguage]['FolderBrowse'])
+            window['-download-'].update(languages[selectedLanguage]['download'])
+            window['-TIPS3-'].update(languages[selectedLanguage]['TIPS3'])
+            window['-TIPS4-'].update(languages[selectedLanguage]['TIPS4'])
+            window['-similarity-'].update(languages[selectedLanguage]['similarity'])
+            window['-author-'].update(languages[selectedLanguage]['author'])
+            window['-copyUrl-'].update(languages[selectedLanguage]['copyUrl'])
+            window['-TIPS5-'].update(languages[selectedLanguage]['TIPS5'])
+            window['-TIPS6-'].update(languages[selectedLanguage]['TIPS6'])
+            window['-TIPS7-'].update(languages[selectedLanguage]['TIPS7'])
+            window['-rename-'].update(languages[selectedLanguage]['rename'])
+            if expectedName == 'fileName':
+                window['-fileName-'].update(languages[selectedLanguage]['filename'])
+            else:
+                window['-pixivName-'].update(languages[selectedLanguage]['pixivname'])
+
+        if event == "-fileName-":
+            expectedName = 'fileName'
+            window['-fileName-'].update(languages[selectedLanguage]['filename'], button_color=('white', "#082567"))
+            window['-pixivName-'].update("    ", button_color=('#082567', "#082567"))
+
+        if event == "-pixivName-":
+            expectedName = 'pixivName'
+            window['-fileName-'].update("    ", button_color=('#082567', "#082567"))
+            window['-pixivName-'].update(languages[selectedLanguage]['pixivname'], button_color=('white', "#082567"))
 
         if event == "-copyUrl-":
             pyperclip.copy(values["-URL-"])
@@ -178,24 +203,24 @@ async def run():
                 rimg.save(rimg_buffer, format='PNG')
                 rbyte_data = rimg_buffer.getvalue()
                 rb64f = base64.b64encode(rbyte_data)
-                similarity_message = f"相似度:{resp.raw[0].similarity}%" if selected_language == 'Chinese' else f"Similarity:{resp.raw[0].similarity}%"
+                similarity_message = f"相似度:{resp.raw[0].similarity}%" if selectedLanguage == 'Chinese' else f"Similarity:{resp.raw[0].similarity}%"
                 window["-similarity-"].update(similarity_message)
-                author_message = f"作者:{resp.raw[0].author}" if selected_language == 'Chinese' else f"Author:{resp.raw[0].author}"
+                author_message = f"作者:{resp.raw[0].author}" if selectedLanguage == 'Chinese' else f"Author:{resp.raw[0].author}"
                 window["-author-"].update(author_message)
-                remaining_message = f"剩余访问额度: 每天:{remaining[1]} 每30秒:{remaining[0]}" if selected_language == 'Chinese' else f"Remaining access credit: One day:{remaining[1]} 30s:{remaining[0]}"
+                remaining_message = f"剩余访问额度: 每天:{remaining[1]}  每30秒:{remaining[0]}" if selectedLanguage == 'Chinese' else f"Remaining access credit: One day:{remaining[1]}  30s:{remaining[0]}"
                 window["-remaining-"].update(remaining_message)
                 window["-URL-"].update(resp.raw[0].url)
                 window["-IMAGE2-"].update(data=rb64f)
             except BaseException as e:
                 print("出错了")
                 print(e.args)
-                window["-remaining-"].update("出错了" if selected_language == 'Chinese' else 'Something went wrong')
+                window["-remaining-"].update("出错了" if selectedLanguage == 'Chinese' else 'Something went wrong')
 
         if event == "-download-":  # 下载时不显示正在下载的图片
             # 打开文件夹
             path = values["-FOLDER-"]
             dirs = os.listdir(path)
-            window["-remaining-"].update("正在下载，请稍候" if selected_language == 'Chinese' else 'Please wait')
+            window["-remaining-"].update("正在下载，请稍候" if selectedLanguage == 'Chinese' else 'Please wait')
             # 搜索并保存图片
             for file in dirs:
                 if file[-3:] not in ["jpg", "png"]:
@@ -206,46 +231,19 @@ async def run():
                     time.sleep(30)
                 if resp.long_remaining == 0:
                     window["-remaining-"].update(
-                        "每日访问额度用完了" if selected_language == 'Chinese' else 'The daily access credit is running out')  # 原来是8层缩进,现在缩减到了5层
+                        "每日访问额度用完了" if selectedLanguage == 'Chinese' else 'The daily access credit is running out')  # 之前是8层缩进,现在缩减到了5层
                     break
-                if "pixiv.net" in resp.raw[0].url:
-                    await GetPixivImg(file, resp.raw[0].url)  # 异步调用GetPixivImg
+                if "pixiv.net" not in resp.raw[0].url:
+                    break
+                if expectedName == 'fileName':
+                    await getPixivImg(resp.raw[0].url, file)  # 异步调用GetPixivImg
+                else:
+                    await getPixivImg(resp.raw[0].url)          # 图片命名为原Pixiv名
             else:  # for循环正常结束
-                window["-remaining-"].update("下载结束" if selected_language == 'Chinese' else 'The download ends')
-
-            # 还是不会用线程
-            # BatchDownloads_thread = threading.Thread(target=BatchDownloads, args=(path, dirs))
-            # BatchDownloads_thread.daemon = True
-            # BatchDownloads_thread.start()
-
-        # if downloadcomplete:
-        #     window["-remaining-"].update(downloadremaining)
-        #     downloadcomplete = False
+                window["-remaining-"].update("下载结束" if selectedLanguage == 'Chinese' else 'The download ends')
 
     window.close()
 
-
-# async def BatchDownloads(path, dirs):         # 线程废案
-#     global downloadremaining, downloadcomplete
-#     for file in dirs:
-#         if file[-3:] not in ["jpg", "png"]:
-#             continue
-#         filename = path + '/' + file
-#         resp = await ImgSearch(filename, apikey)
-#         if resp.short_remaining == 0:
-#             time.sleep(30)
-#         if resp.long_remaining == 0:
-#             # window["-remaining-"].update("每日访问额度用完了")  # 原来是8层缩进,现在缩减到了5层
-#             downloadremaining = "每日访问额度用完了"
-#             downloadcomplete = True
-#             return
-#         if "pixiv.net" in resp.raw[0].url:
-#             await GetPixivImg(file, resp.raw[0].url)  # 异步调用GetPixivImg
-#     else:  # for循环正常结束
-#         # window["-remaining-"].update("下载结束")
-#         downloadremaining = "下载结束"
-#         downloadcomplete = True
-#         return
 
 if __name__ == "__main__":
     asyncio.run(run())
